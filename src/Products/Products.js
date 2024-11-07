45a14075e94a98a123eb10c3c5dc901d11c4c14f
bhavanasingh01 <bhavana0105singh@gmail.com>
2024-11-07 09:23:15 +0530

    first commit

---
+++ src/Products/Products.js
@@ -0,0 +1,9 @@
+import "./Products.css";
+const Products = ({ result }) => {
+  return (
+    <>
+      <section className="card-container">{result}</section>
+    </>
+  );
+};
+export default Products;
